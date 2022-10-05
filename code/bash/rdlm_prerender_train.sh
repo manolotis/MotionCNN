52af10840d7e7f177fbb1e36906e6d36b081ca50
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-05 11:51:13 +0200

    Fix dataset path after moving to HDD

--- code/bash/rdlm_prerender_train.sh
+++ code/bash/rdlm_prerender_train.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=-1 python /home/manolotis/sandbox/robustness_benchmark/motionCNN/prerender.py \
-    --data "/home/manolotis/datasets/waymo/motion v1.0/uncompressed/tf_example/training/" \
+    --data "/media/disk1/datasets/waymo/motion v1.0/uncompressed/tf_example/training/" \
     --out "/home/manolotis/datasets/waymo/motion v1.0/prerender/motionCNN/training/" \
     --n-shards 1 \
     --n-jobs 48
