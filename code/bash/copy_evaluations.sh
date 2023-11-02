1c78d1a2d70d0d1f4e9ed431675d0b866e34fb76
manolotis <manuel.munoz.sanchez@gmail.com>
2023-11-02 17:43:04 +0100

    copy evaluations script

--- code/bash/copy_evaluations.sh
+++ code/bash/copy_evaluations.sh
@@ -1,3 +1,3 @@
 scp -pr \
-  manolotis@rdlm.wtb.tue.nl:/home/manolotis/sandbox/robustness_benchmark/motionCNN/evaluations/ \
-  /home/manolotis/sandbox/robustness_benchmark/motionCNN/
+  manolotis@rdlm.wtb.tue.nl:/home/manolotis/sandbox/scenario_based_evaluation/motionCNN/evaluations/ \
+  /home/manolotis/sandbox/scenario_based_evaluation/motionCNN/
